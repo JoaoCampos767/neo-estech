424669a6d09044228013d2ba0f2966ef00f88c15
JoaoCampos767 <joaopedro767767@gmail.com>
2022-09-15 23:44:12 -0300

    Ajuste/Melhoria

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,69 @@
+import { Menu } from "antd";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Table from "../Table";
+import "./styles.css";
+
+const items = [
+  {
+    label: (
+      <a target="_blank" rel="noopener noreferrer">
+        Tabela de Chamados
+        <Link to={{ pathname: "/table" }} />
+      </a>
+    ),
+    key: "table",
+  },
+  {
+    label: (
+      <a target="_blank" rel="noopener noreferrer">
+        Gráfico
+        <Link to={{ pathname: "/chart" }} />
+      </a>
+    ),
+    key: "chart",
+  },
+  {
+    label: (
+      <a target="_blank" rel="noopener noreferrer">
+        Status da Estação
+        <Link to={{ pathname: "/card" }} />
+      </a>
+    ),
+    key: "card",
+  },
+  {
+    label: (
+      <a target="_blank" rel="noopener noreferrer">
+        Log in
+        <Link to={{ pathname: "/" }} />
+      </a>
+    ),
+    key: "login",
+  },
+];
+
+const App = () => {
+  const [current, setCurrent] = useState("mail");
+
+  const onClick = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
+};
+
+export default App;
+
+/*
 import { Button } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
@@ -27,8 +93,5 @@ export default function Btn() {
       </div>
     </>
   );
-}
-
-/*
-
+}/
 */
